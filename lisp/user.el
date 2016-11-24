9df2c0ab857c38a6b880efd73087e8d4a5b0db78
Evgeny Cherkashin <eugene@irnok.net>
2016-11-24 14:08:44 +0800

    Fought with linum mode. Seems won.

--- lisp/user.el
+++ lisp/user.el
@@ -169,6 +169,8 @@
 
 ;(require 'linum)
 
+(require 'linum+)
+
 ;;(global-linum-mode 1)
 
 (global-company-mode 1)
@@ -180,14 +182,25 @@
 
 ;(load-file (expand-file-name "epy/epy-init.el" dotfiles-dir))
 
-(if
-    t ;;windowed-system
-    (setq linum-format "%4d ")
-  (progn
-    (setq linum-format "%3d ")
-    (global-linum-mode 1)
-    )
-  )
+;; (if
+;;     t ;;windowed-system
+;;     (setq linum-format "%4d ")
+;;   (progn
+;;     (setq linum-format "%3d ")
+;;     (global-linum-mode 1)
+;;     )
+;;   )
+(defun linum-update-window-scale-fix (win)
+  "fix linum for scaled text"
+  (set-window-margins win
+                      (ceiling (* (if (boundp 'text-scale-mode-step)
+                                      (expt text-scale-mode-step
+                                            text-scale-mode-amount) 1)
+                                  (if (car (window-margins))
+                                      (car (window-margins)) 1)
+                                  ))))
+;(advice-add #'linum-update-window :after #'linum-update-window-scale-fix)
+
 
 (autoload 'run-prolog "prolog" "Start a Prolog sub-process." t)
 (autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
